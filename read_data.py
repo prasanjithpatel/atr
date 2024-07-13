22ed0b468ee5ce6cdffd32206c1b227ba531ce38
prasanjithpatel <panduprasanjith@gmail.com>
2024-07-13 11:59:32 +0530

    decoded_sil

--- read_data.py
+++ read_data.py
@@ -8,5 +8,5 @@ read1 = "/home/prasanjith/Desktop/atr/silver/FOREXCOM_XAGUSD_ytd.csv"
 read2 = pd.read_csv(read)
 read3 =pd.read_csv(read1)
 
-read2.to_json("silver_3m", orient='records')
-read3.to_json("siver60",orient="records")
+read2.to_json("hour_day.json", orient='records')
+read3.to_json("one_day.json",orient="records")
