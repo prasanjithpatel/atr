8bc07502165a6eebc2e5d2cc81ed336f9c4fd9fa
prasanjithpatel <panduprasanjith@gmail.com>
2024-08-09 23:14:09 +0530

    bulk_json

--- read_data.py
+++ read_data.py
@@ -2,11 +2,11 @@ import os
 import pandas as pd
 
 
-read = "/home/prasanjith/Desktop/atr/silver/FOREXCOM_XAGUSD_3m.csv"
-read1 = "/home/prasanjith/Desktop/atr/silver/FOREXCOM_XAGUSD_ytd.csv"
+read = "/home/prasanjith/Desktop/atr/Bulk-Deals-09-08-2023-to-09-08-2024.csv"
+#read1 = "/home/prasanjith/Desktop/atr/silver/FOREXCOM_XAGUSD_ytd.csv"
 
 read2 = pd.read_csv(read)
-read3 =pd.read_csv(read1)
+#read3 =pd.read_csv(read1)
 
-read2.to_json("hour_day.json", orient='records')
-read3.to_json("one_day.json",orient="records")
+read2.to_json("Bulk_deals.json", orient='records')
+#read3.to_json("one_day.json",orient="records")
